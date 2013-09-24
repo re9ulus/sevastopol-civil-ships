93456275f7e12f1811082f9a54d945eb7222b048
Basil Zemlyanov <angeldaren@gmail.com>
2013-09-24 22:34:35 +0300

    Add ship search in AIM system

--- src/scrapper.py
+++ src/scrapper.py
@@ -1,6 +1,22 @@
+import urllib2
+from bs4 import BeautifulSoup
+import codecs
+
 class Scrapper:
     '''
     Scrapper class.
     '''
-    def __init__(self):
+    def __init__(self, ais_url = 'http://www.marinetraffic.com/'):
         pass
+
+    def scrapeShip(self, ship_name, ship_type="Passenger"):
+        #url = "http://www.marinetraffic.com/ais/ru/datasheet.aspx?SHIPNAME={0}&TYPE_SUMMARY={1}&menuid=&datasource=SHIPS_CURRENT&app=&mode=&B1=%D0%9F%D0%BE%D0%B8%D1%81%D0%BA".format(ship_name, ship_type)
+        url = r"http://www.marinetraffic.com/ais/datasheet.aspx?SHIPNAME=NORD&TYPE_SUMMARY=Passenger&menuid=&datasource=SHIPS_CURRENT&app=&mode=&B1=Search"
+        html = urllib2.urlopen(url).read()
+        soup = BeautifulSoup(html)
+        print soup.find("a",class_='data',text="SEVASTOPOL").find_parent("tr")
+        # TODO: parse table row
+
+scrapper = Scrapper()
+
+scrapper.scrapeShip('NORD')
