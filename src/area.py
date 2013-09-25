f34b342ae8af4d533c196c3adfaa0db6343f0a6d
Samael500 <Samael500@gmail.com>
2013-09-25 19:03:28 +0300

    Перевод. Добавлен вспомогательный клас угол
    Закончу и проверю работоспособность чуть позже

--- src/area.py
+++ src/area.py
@@ -1,76 +1,52 @@
+from coordinates import Coordinates
+from coordinates import Angle
+
 class Area:
   '''
   Area class.
   '''
   def __init__(self, points []):
     self.points = points
-
-  def __point_in__ (Coordinates x)
+    self.angles = []
+	self.zero = points[0]
 	
-	
-## Emaxx - нужно перевести
-	struct pt {
-	int x, y;
-};
+  def __start_point__ () :
+	'''
+	Перегрупировывает точки многоугольника для обхода по часовой стрелки слева снизу
+	'''
+  	zero_id = 0 # указатель на начальную точку обхода
+	for i in range(len(self.points)) :
+		if (points[i].latitude < points[zero_id].latitude) or (points[i].latitude == points[zero_id].latitude) and (points[i].longitude < points[zero_id].longitude) :
+			zero_id = i
 
-struct ang {
-	int a, b;
-};
-
-bool operator < (const ang & p, const ang & q) {
-	if (p.b == 0 && q.b == 0)
-		return p.a < q.a;
-	return p.a * 1ll * q.b < p.b * 1ll * q.a;
-}
-
-long long sq (pt & a, pt & b, pt & c) {
-	return a.x*1ll*(b.y-c.y) + b.x*1ll*(c.y-a.y) + c.x*1ll*(a.y-b.y);
-}
-
-int main() {
-
-	int n;
-	cin >> n;
-	vector<pt> p (n);
-	int zero_id = 0;
-	for (int i=0; i<n; ++i) {
-		scanf ("%d%d", &p[i].x, &p[i].y);
-		if (p[i].x < p[zero_id].x || p[i].x == p[zero_id].x && p[i].y < p[zero_id].y)
-			zero_id = i;
-	}
-	pt zero = p[zero_id];
-	rotate (p.begin(), p.begin()+zero_id, p.end());
-	p.erase (p.begin());
-	--n;
-
-	vector<ang> a (n);
-	for (int i=0; i<n; ++i) {
-		a[i].a = p[i].y - zero.y;
-		a[i].b = p[i].x - zero.x;
-		if (a[i].a == 0)
-			a[i].b = a[i].b < 0 ? -1 : 1;
-	}
-
-	for (;;) {
-		pt q; // очередной запрос
-		bool in = false;
-		if (q.x >= zero.x)
-			if (q.x == zero.x && q.y == zero.y)
-				in = true;
-			else {
-				ang my = { q.y-zero.y, q.x-zero.x };
-				if (my.a == 0)
-					my.b = my.b < 0 ? -1 : 1;
-				vector<ang>::iterator it = upper_bound (a.begin(), a.end(), my);
-				if (it == a.end() && my.a == a[n-1].a && my.b == a[n-1].b)
-					it = a.end()-1;
-				if (it != a.end() && it != a.begin()) {
-					int p1 = int (it - a.begin());
-					if (sq (p[p1], p[p1-1], q) <= 0)
-						in = true;
-				}
-			}
-		puts (in ? "INSIDE" : "OUTSIDE");
-	}
-
-}
+	zero = points[zero_id]
+	## warning 	rotate (p.begin(), p.begin()+zero_id, p.end()); p.erase (p.begin());
+	points = points[zero_id+1:] + points[:zero_id]	## Кажется правильно делаю? поменять порядок элементов и кильнуть первый?
+	for i in range(len(self.points)) :
+		angles[i].rho = points[i].longitude - zero.longitude
+		angles[i].phi = points[i].latitude - zero.latitude
+			if (angles[i].rho == 0) :
+				angles[i].phi = [1, -1] [angles[i].phi < 0]
+  
+  def __das_is_in__ (point) 
+    '''
+	Определяет принадленжность точки
+	'''
+	is_in = False
+	if (point.latitude >= zero.latitude) :
+		if (point.latitude == zero.latitude) and (point.longitude == zero.longitude) :
+			is_in = True
+		else :
+			angle.rho = point.longitude-zero.longitude
+			angle.phi = point.latitude-zero.latitude
+			if (angle.rho == 0):
+				angle.phi = [1, -1] [angle.phi  < 0]	
+			
+			vector<ang>::iterator it = upper_bound (a.begin(), a.end(), my);
+			if (it == a.end() && my.a == a[n-1].a && my.b == a[n-1].b)
+				it = a.end()-1;
+			if (it != a.end() && it != a.begin()) {
+				int p1 = int (it - a.begin());
+				if (sq (p[p1], p[p1-1], q) <= 0)
+					in = true;
+	return ["OUTSIDE", "INSIDE"] [is_in]
--- src/coordinates.py
+++ src/coordinates.py
@@ -5,3 +5,20 @@ class Coordinates:
   def __init__(self,latitude,longitude):
     self.latitude = latitude
     self.longitude = longitude
+
+  def __square__ (a, b, c)
+	return a.latitude*(b.longitude-c.longitude) + b.latitude*(c.longitude-a.longitude) + c.latitude*(a.longitude-b.longitude)
+
+class Angle
+  '''
+  Angle class.
+  '''
+  def __init__(self, rho, phi):
+    self.rho = rho
+    self.phi = phi  
+  
+  def __lt__ (self, point)
+    if (self.phi == 0) and (point.phi == 0) :
+		return (self.rho < point.rho)
+	return (self.rho * point.phi) < (self.phi * point.rho)
+ 	
