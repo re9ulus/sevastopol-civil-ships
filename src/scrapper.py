ea5fe92dd40b27000439300035edf8716322843f
Basil Zemlyanov <angeldaren@gmail.com>
2013-09-27 22:33:06 +0300

    Update scrapper.py

--- src/scrapper.py
+++ src/scrapper.py
@@ -12,7 +12,7 @@ class Scrapper:
     def scrape_ship(self, ship_name):
         '''
         Scrape ship data from AIS.
-        return: ( speed, course, (lattitude, longitude) )
+        return: ( speed, course, (latitude, longitude) )
         '''
         url = r"http://www.marinetraffic.com/ais/datasheet.aspx?datasource=SHIPS_CURRENT&PORT_ID=883&SHIPNAME={0}".format(ship_name)
         try:
@@ -20,12 +20,12 @@ class Scrapper:
             data = soup.find("a",class_='data',text=ship_name).find_parent("tr").find_all("td")
             speed = float(data[3].text)
             course = float(data[4].text)
-            raw_position = data[7].find("a")["href"]
+            raw_position = data[9].find("a")["href"]
             pattern = re.compile(r"centerx=([0-9\.]+)&centery=([0-9\.]+)")
             m = pattern.search(raw_position)
             longitude = float(m.group(1))
-            lattitude = float(m.group(2))
-            res = ( speed, course, (lattitude, longitude) )
+            latitude = float(m.group(2))
+            res = ( speed, course, (latitude, longitude) )
         except (AttributeError, urllib2.URLError):
             res = None
         finally:
