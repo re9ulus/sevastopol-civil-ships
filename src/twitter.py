8061b31215076126974dc4983933737ac420f9fe
Basil Zemlyanov <angeldaren@gmail.com>
2013-09-28 19:07:18 +0300

    Add twitter.py

---
+++ src/twitter.py
@@ -0,0 +1,21 @@
+# TODO: Select python library with twitter api
+
+class Twitter:
+    '''
+    Twitter class.
+    '''
+    def __init__(self):
+        pass
+
+    def __login__(self, data):
+        '''
+        Twitter login with OAuth2.
+        data - tokens and other login info.
+        '''
+        pass
+
+    def write_tweet(self, message):
+        '''
+        Writes message into twitter stream.
+        '''
+        pass
