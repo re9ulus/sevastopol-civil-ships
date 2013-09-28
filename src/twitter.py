6a4b0f50133a1636183bfc79e30b8474c5508711
Basil Zemlyanov <angeldaren@gmail.com>
2013-09-28 20:14:52 +0300

    Add Twython lib, post_tweet method

--- src/twitter.py
+++ src/twitter.py
@@ -1,21 +1,16 @@
-# TODO: Select python library with twitter api
+from twython import Twython
+import twitter_oauth_info as auth_data
 
 class Twitter:
     '''
     Twitter class.
     '''
-    def __init__(self):
-        pass
+    def __init__(self, auth=auth_data):
+        self.twitter = Twython(auth.APP_KEY, auth.APP_SECRET,
+                  auth.OAUTH_TOKEN, auth.OAUTH_TOKEN_SECRET)
 
-    def __login__(self, data):
-        '''
-        Twitter login with OAuth2.
-        data - tokens and other login info.
-        '''
-        pass
-
-    def write_tweet(self, message):
+    def post_tweet(self, message):
         '''
         Writes message into twitter stream.
         '''
-        pass
+        self.twitter.update_status(status=message)
